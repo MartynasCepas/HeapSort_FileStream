d521c0f482fff118070e2f44db6c052df51b7692
MartynasCepas <martynas.cepas@ktu.edu>
2020-04-05 19:30:24 +0300

    Added sorting with linked lists

---
+++ HeapSort_File/List.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeapSort_File
+{
+    class MyFileList : DataList
+    {
+        int prevNode;
+        int currentNode;
+        int nextNode;
+        public MyFileList(string filename, int n, int seed)
+        {
+            length = n;
+            Random rand = new Random(seed);
+            if (File.Exists(filename)) File.Delete(filename);
+            try
+            {
+                using (BinaryWriter writer = new BinaryWriter(File.Open(filename,
+                    FileMode.Create)))
+                {
+                    writer.Write(4);
+                    for (int j = 0; j < length; j++)
+                    {
+                        writer.Write(rand.NextDouble());
+                        writer.Write((j + 1) * 12 + 4);
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+        public FileStream fs { get; set; }
+        public override double Head()
+        {
+            Byte[] data = new Byte[12];
+            fs.Seek(0, SeekOrigin.Begin);
+            fs.Read(data, 0, 4);
+            currentNode = BitConverter.ToInt32(data, 0);
+            prevNode = -1;
+            fs.Seek(currentNode, SeekOrigin.Begin);
+            fs.Read(data, 0, 12);
+            double result = BitConverter.ToDouble(data, 0);
+            nextNode = BitConverter.ToInt32(data, 8);
+            return result;
+        }
+        public override double Next()
+        {
+            Byte[] data = new Byte[12];
+            fs.Seek(nextNode, SeekOrigin.Begin);
+            fs.Read(data, 0, 12);
+            prevNode = currentNode;
+            currentNode = nextNode;
+            double result = BitConverter.ToDouble(data, 0);
+            nextNode = BitConverter.ToInt32(data, 8);
+            return result;
+        }
+        public override void Swap(int i, int j)
+        {
+            Byte[] data;
+            double a = IndexAt(i);
+            double b = IndexAt(j); 
+
+            var temp = IndexAt(j);
+            data = BitConverter.GetBytes(a);
+            fs.Seek(currentNode, SeekOrigin.Begin);
+            fs.Write(data, 0, 8);
+
+            temp = IndexAt(i);
+            data = BitConverter.GetBytes(b);
+            fs.Seek(currentNode, SeekOrigin.Begin);
+            fs.Write(data, 0, 8);
+        }
+
+        public override double IndexAt(int index)
+        {
+            //Index was negative or larger then the amount of Nodes in the list
+            if (index < 0 || index > length)
+                return 0;
+            var tmp = Head();
+            // Move to index
+            for (int i = 0; i < index; i++)
+            {
+                tmp = Next();
+            }
+            // return the node at the index position
+            return tmp;
+        }
+    }
+}
--- HeapSort_File/Program.cs
+++ HeapSort_File/Program.cs
@@ -17,67 +17,118 @@ namespace HeapSort_File
             Test_File_Array_List(seed);
         }
 
-    
-        public static void Heapify(DataArray arr, int n, int i) 
+
+        public static void Heapify(DataArray arr, int n, int i)
+        {
+            int largest = i; // Initialize largest as root 
+            int l = 2 * i + 1; // left = 2*i + 1 
+            int r = 2 * i + 2; // right = 2*i + 2 
+
+            // If left child is larger than root 
+            if (l < n && arr[l] > arr[largest])
+                largest = l;
+
+            // If right child is larger than largest so far 
+            if (r < n && arr[r] > arr[largest])
+                largest = r;
+
+            // If largest is not root 
+            if (largest != i)
+            {
+                arr.Swap(i, largest, arr[i], arr[largest]);
+
+                // Recursively heapify the affected sub-tree 
+                Heapify(arr, n, largest);
+            }
+        }
+
+        public static void Heapify(DataList arr, int n, int i) 
         { 
             int largest = i; // Initialize largest as root 
             int l = 2*i + 1; // left = 2*i + 1 
             int r = 2*i + 2; // right = 2*i + 2 
   
             // If left child is larger than root 
-            if (l < n && arr[l] > arr[largest]) 
+            if (l < n &&  (arr.IndexAt(1) != 0) && arr.IndexAt(l)  > arr.IndexAt(largest)) 
                 largest = l; 
   
             // If right child is larger than largest so far 
-            if (r < n && arr[r] > arr[largest]) 
+            if (r < n && (arr.IndexAt(1) != 0) && arr.IndexAt(r) > arr.IndexAt(largest)) 
                 largest = r; 
   
             // If largest is not root 
             if (largest != i) 
             {
-                arr.Swap(i, largest, arr[i],arr[largest]);
+                arr.Swap(i, largest);
   
                 // Recursively heapify the affected sub-tree 
                 Heapify(arr, n, largest); 
             } 
         }
-        
+
         public static void HeapSort(DataArray items)
         {
-            int n = items.Length; 
-  
+            int n = items.Length;
+
             // Build heap (rearrange array) 
-            for (int i = n / 2 - 1; i >= 0; i--) 
-                Heapify(items, n, i); 
-  
+            for (int i = n / 2 - 1; i >= 0; i--)
+                Heapify(items, n, i);
+
             // One by one extract an element from heap 
-            for (int i=n-1; i>=0; i--) 
-            { 
+            for (int i = n - 1; i >= 0; i--)
+            {
                 // Move current root to end 
 
                 items.Swap(0, i, items[0], items[i]);
+
+                // call max heapify on the reduced heap 
+                Heapify(items, i, 0);
+            }
+        }
+
+        public static void HeapSort(DataList items)
+        {
+            int n = items.Length;
+            
+            for (int i = n / 2 - 1; i >= 0; i--) 
+                Heapify(items, n, i); 
+
+            for (int i=n-1; i>=0; i--) 
+            { 
+                // Move current root to end 
+                items.Swap(0, i);
   
                 // call max heapify on the reduced heap 
                 Heapify(items, i, 0); 
-            } 
+            }
         }
 
-    public static void Test_File_Array_List(int seed)
-    {
-        int n = 12;
-        string filename;
-        filename = @"mydataarray.dat";
-        MyFileArray myfilearray = new MyFileArray(filename, n, seed);
-        using (myfilearray.fs = new FileStream(filename, FileMode.Open,
-       FileAccess.ReadWrite))
+        public static void Test_File_Array_List(int seed)
         {
-            Console.WriteLine("\n FILE ARRAY \n");
-            myfilearray.Print(n);
-            HeapSort(myfilearray);
-            myfilearray.Print(n);
+            int n = 12;
+            string filename;
+            filename = @"mydataarray.dat";
+            MyFileArray myfilearray = new MyFileArray(filename, n, seed);
+            using (myfilearray.fs = new FileStream(filename, FileMode.Open,
+           FileAccess.ReadWrite))
+            {
+                Console.WriteLine("\n FILE ARRAY \n");
+                myfilearray.Print(n);
+                HeapSort(myfilearray);
+                myfilearray.Print(n);
+            }
+            filename = @"mydatalist.dat";
+            MyFileList myfilelist = new MyFileList(filename, n, seed);
+            using (myfilelist.fs = new FileStream(filename, FileMode.Open,
+                FileAccess.ReadWrite))
+            {
+                Console.WriteLine("\n FILE LIST \n");
+                myfilelist.Print(n);
+                HeapSort(myfilelist);
+                myfilelist.Print(n);
+            }
+
         }
-        
-    }
     }
 }
 abstract class DataArray
@@ -93,5 +144,23 @@ abstract class DataArray
         Console.WriteLine();
     }
 }
+
+abstract class DataList
+{
+    protected int length;
+    public int Length { get { return length; } }
+    public abstract double Head();
+    public abstract double Next();
+    public abstract void Swap(int a, int b);
+    public abstract double IndexAt(int index);
+    public void Print(int n)
+    {
+        Console.Write(" {0:F5} ", Head());
+        for (int i = 1; i < n; i++)
+            Console.Write(" {0:F5} ", Next());
+        Console.WriteLine();
+    }
+}
+
 
 
